c2499d420adfee05822ad3a7eb286730ac35a630
Willyan Gabriel <145140640+Willyang10x@users.noreply.github.com>
2025-05-17 15:00:06 -0300

    Adicionando barra hamburguer
    Reponsividade do site

--- new.js
+++ new.js
@@ -1,19 +1,10 @@
 particlesJS('particles-js', {
   particles: {
-    number: {
-      value: 60,
-      density: { enable: true, value_area: 800 }
-    },
+    number: { value: 60, density: { enable: true, value_area: 800 } },
     color: { value: "#00bcd4" },
     shape: { type: "circle" },
-    opacity: {
-      value: 0.5,
-      random: false
-    },
-    size: {
-      value: 3,
-      random: true
-    },
+    opacity: { value: 0.5, random: false },
+    size: { value: 3, random: true },
     line_linked: {
       enable: true,
       distance: 150,
@@ -21,28 +12,21 @@ particlesJS('particles-js', {
       opacity: 0.4,
       width: 1
     },
-    move: {
-      enable: true,
-      speed: 3
-    }
+    move: { enable: true, speed: 3 }
   },
   interactivity: {
     detect_on: "canvas",
     events: {
-      onhover: {
-        enable: true,
-        mode: "repulse"
-      },
-      onclick: {
-        enable: false
-      },
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: false },
       resize: true
     },
-    modes: {
-      repulse: {
-        distance: 100
-      }
-    }
+    modes: { repulse: { distance: 100 } }
   },
   retina_detect: true
+});
+
+
+document.getElementById('hamburger').addEventListener('click', function () {
+  document.getElementById('nav-menu').classList.toggle('show');
 });
