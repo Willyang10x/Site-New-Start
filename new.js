6c99e600bacb01835f7da32ea35cbd9cba99722a
Willyan Gabriel <145140640+Willyang10x@users.noreply.github.com>
2025-05-17 12:29:00 -0300

    Criação de efeito no fundo do site

---
+++ new.js
@@ -0,0 +1,48 @@
+particlesJS('particles-js', {
+  particles: {
+    number: {
+      value: 60,
+      density: { enable: true, value_area: 800 }
+    },
+    color: { value: "#00bcd4" },
+    shape: { type: "circle" },
+    opacity: {
+      value: 0.5,
+      random: false
+    },
+    size: {
+      value: 3,
+      random: true
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#00bcd4",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 3
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: false
+      },
+      resize: true
+    },
+    modes: {
+      repulse: {
+        distance: 100
+      }
+    }
+  },
+  retina_detect: true
+});
